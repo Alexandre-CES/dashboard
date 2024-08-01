949d756a12a74be6ffb834ccf90684056f10dca4
Alexandre-CES <147560376+Alexandre-CES@users.noreply.github.com>
2024-08-01 18:33:02 -0300

    starting light/dark theme idea

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-import csv
-
 #Config page
 st.set_page_config(
     page_title="Pokemon Dashboard",
@@ -22,23 +20,54 @@ def load_data():
     df = pd.read_csv('./data/Pokemon.csv')
     return df
 
-df = load_data()
-
-#Title
-st.title("Pokemon Dashboard")
-
 #Sidebar
-with st.sidebar:
-    st.header("Informações do Aluno")
-    st.write("Nome Completo: Alexandre Cabral do Espírito Santo")
-    st.write("PDITA: PD030")
+def display_sidebar(df):
+    with st.sidebar:
+        st.header("Informações do Aluno")
+        st.write("Nome Completo: Alexandre Cabral do Espírito Santo")
+        st.write("PDITA: PD030")
 
-    selected_generation = st.selectbox("Selecione a Geração", df['Generation'].unique())
-    selected_type = st.selectbox("Selecione o Tipo", df['Type 1'].unique())
-    total_range = st.slider("Selecione a Faixa de Estatística total", int(df['Total'].min()), int(df['Total'].max()), (50, 100))
+        selected_generation = st.selectbox("Selecione a Geração", df['Generation'].unique())
+        selected_type = st.selectbox("Selecione o Tipo", df['Type 1'].unique())
+        total_range = st.slider("Selecione a Faixa de Estatística total", int(df['Total'].min()), int(df['Total'].max()), (50, 780))
 
-filtered_df = df[(df['Generation'] == selected_generation) & (df['Type 1'] == selected_type) & (df['Total'] >= total_range[0]) & (df['Total'] <= total_range[1])]
+    return selected_generation, selected_type, total_range
 
-# Exemplo de gráfico
-fig = px.scatter_3d(filtered_df, x='Type 1', y='Type 2', z='Total', animation_frame='Generation', color='Total', title="Mistura de tipos com maior ataque")
-st.plotly_chart(fig)
+#filtrar com os requisitos selecionados
+def filter_data(df, generation, p_type, total_range):
+    filtered_df = df[(df['Generation'] == generation) & (df['Type 1'] == p_type) & (df['Total'] >= total_range[0]) & (df['Total'] <= total_range[1])]
+    return filtered_df
+
+#Gráfico
+def create_chart(df):
+    fig = px.scatter_3d(
+        df,
+        x='Type 1',
+        y='Type 2',
+        z='Total',
+        animation_frame='Generation',
+        color='Total',
+        title="Mistura de tipos com maior total de estatísticas",
+        hover_data={'Name': True}
+    )
+    st.plotly_chart(fig)
+
+
+def main():
+    # Carregar dados
+    df = load_data()
+    
+    # Exibir título
+    st.title("Pokemon Dashboard")
+    
+    # Exibir barra lateral
+    selected_generation, selected_type, total_range = display_sidebar(df)
+    
+    # Filtrar dados
+    filtered_df = filter_data(df, selected_generation, selected_type, total_range)
+    
+    # Criar gráfico
+    create_chart(filtered_df)
+
+if __name__ == "__main__":
+    main()
